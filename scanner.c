181ea9a12ed9369ebdc2d0d8ceb9d7009624a107
mbertovic <mb.bertovic@gmail.com>
2013-10-15 12:39:52 +0200

    basic scanner implementation
    function scannerGetToken is returning only type of token, not the
    content, however, it is prepared.
    
    type double is working with "." , not with exponent "e" yet
    
    comparing identifiers with keywords table is also not working

---
+++ scanner.c
@@ -0,0 +1,300 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "scanner.h"
+
+int32_t charStreamSwitch = 1;
+int32_t lastChar;
+FILE* source;
+
+ScannerTokenType scannerGetToken()
+{
+    ScannerTokenType token = STT_Empty;
+    ScannerTokenState tokenState = STS_NOT_FINISHED;
+    ScannerState state = SS_Empty;
+    int32_t symbol;
+
+    while ( tokenState == STS_NOT_FINISHED )
+    {
+        if ( charStreamSwitch ) {
+            if ( ( symbol = getc(source) ) == EOF ) {
+                return STT_EOF;
+            }
+        }
+        else {
+            symbol = lastChar;
+            charStreamSwitch=1;
+        }
+        switch ( state ) {
+            case SS_Empty : {
+                if (( symbol >= 'a' && symbol <= 'z') || ( symbol >= 'A' && symbol <= 'Z')) {
+                    state = SS_Identifier;
+                }
+                else if ( symbol >= '0' && symbol <= '9')  {
+                    state = SS_Number;
+                }
+                else if ( isspace( symbol ) )
+                {
+                    ;
+                }
+                else {
+                    switch ( symbol ) {
+                        case '"' : {
+                            state = SS_String;
+                            break;
+                        }
+                        case '/' : {
+                            state = SS_Divide;
+                            break;
+                        }
+                        case '!' : {
+                            state = SS_Exclamation;
+                            break;
+                        }
+                        case ';' : {
+                            return STT_Semicolon;
+                            break;
+                        }
+                        case '{' : {
+                            return STT_LeftCurlyBracket;
+                            break;
+                        }
+                        case '}' : {
+                            return STT_RightCurlyBracket;
+                            break;
+                        }
+                        case '*' : {
+                            return STT_Asterisk;
+                            break;
+                        }
+                        case '+' : {
+                            return STT_Plus;
+                            break;
+                        }
+                        case '-' : {
+                            return STT_Minus;
+                            break;
+                        }
+                        case '(' : {
+                            return STT_LeftBracket;
+                            break;
+                        }
+                        case ')' : {
+                            return STT_RightBracket;
+                            break;
+                        }
+                        case ',' : {
+                            return STT_Comma;
+                            break;
+                        }
+                        case ':' : {
+                            return STT_Colon;
+                            break;
+                        }
+                        case '>' : {
+                            state = SS_Greater;
+                            break;
+                        }
+                        case '<' : {
+                            state = SS_Less;
+                            break;
+                        }
+                        case '=' : {
+                            state = SS_Assigment;
+                            break;
+                        }
+                        case '$' : {
+                            state = SS_Dollar;
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+            case SS_Greater : {
+                if ( symbol == '=' ) {
+                    return STT_GreaterEqual;
+                }
+                else {
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    return STT_Greater;
+                }
+            }
+            case SS_Less : {
+                if ( symbol == '=' ) {
+                    return STT_LessEqual;
+                }
+                else {
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    return STT_Less;
+                }
+            }
+            case SS_Assigment : {
+                if ( symbol == '=' ) {
+                    state = SS_Equal;
+                    break;
+                }
+                else {
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    return STT_Assigment;
+                }
+            }
+            case SS_Dollar : {
+                if ( ( symbol >= 'a' && symbol <= 'z' ) || ( symbol >= 'A' && symbol <= 'Z' ) || ( symbol == '_' ) ) {
+                    state = SS_Variable;
+                    // STRING PUSH
+                    break;
+                }
+                else {
+                    return STT_LexError;// LEX ERROR ???
+                }
+            }
+            case SS_Variable : {
+                if ( ( symbol >= 'a' && symbol <= 'z' ) || ( symbol >= 'A' && symbol <= 'Z' ) || ( symbol == '_' ) ||
+                     ( symbol >= '0' && symbol <= '9' )) {
+                    // STRING PUSH
+                    state = SS_Variable; // STILL THE SAME STATE
+                    break;
+                }
+                else {
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    return STT_Variable; // PLUS RETURN STRING SOMEWHERE
+                }
+            }
+            case SS_Number : {
+                if ( symbol >= '0' && symbol <= '9') {
+                    state = SS_Number;
+                    // PUSH STRING
+                    break;
+                }
+                else if ( symbol == '.') {
+                    state = SS_Double;
+                }
+                // ELSE IF EXPONENT
+                else {
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    return STT_Number;
+                }
+            }
+            case SS_Identifier : {
+                if ( ( symbol == '_' ) || ( symbol >= 'a' && symbol <= 'z') || ( symbol >= ' A' && symbol <= 'Z' ) || ( symbol >= '0' && symbol <= '9') ) {
+                    state = SS_Identifier;
+                    break;
+                }
+                else {
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    return STT_Identifier;
+                }
+            }
+            case SS_Double : {
+                if ( symbol >= '0' && symbol <= '9') {
+                    state = SS_Double;
+                    // PUSH STRING
+                    break;
+                }
+                else {
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    return STT_Double;
+                }
+            }
+            case SS_Divide : {
+                if ( symbol == '*' ) {
+                    state = SS_BlockComment;
+                    break;
+                }
+                else if ( symbol == '/' ) {
+                    state = SS_Comment;
+                    break;
+                }
+                else {
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    return STT_Divide;
+                }
+            }
+            case SS_BlockComment : {
+                if ( symbol == '*' ) {
+                    state = SS_BlockCommentFinish;
+                    break;
+                }
+                else {
+                    state = SS_BlockComment;
+                    break;
+                }
+            }
+            case SS_BlockCommentFinish : {
+                if ( symbol == '/' ) {
+                    state = SS_Empty;
+                    break;
+                }
+                else {
+                    state = SS_BlockComment;
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    break;
+                }
+            }
+            case SS_Comment : {
+                if ( symbol == '\n') {
+                    state = SS_Empty;
+                    break;
+                }
+                else {
+                    state = SS_Comment;
+                    break;
+                }
+            }
+            case SS_Equal : {
+                if ( symbol == '=' ) {
+                    return STT_Equal;
+                }
+                else {
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    return STT_LexError;
+                }
+            }
+            case SS_Exclamation : {
+                if ( symbol == '=' ) {
+                    state = SS_NotEqual;
+                    break;
+                }
+                else {
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    return STT_LexError;
+                }
+            }
+            case SS_NotEqual : {
+                if ( symbol == '=' ) {
+                    return STT_NotEqual;
+                }
+                else {
+                    lastChar = symbol;
+                    charStreamSwitch = 0;
+                    return STT_LexError;
+                }
+            }
+            case SS_String : {
+                if ( symbol == '"' ) {
+                    return STT_String;
+                }
+                else {
+                    state = SS_String;
+                    // CONTENT OF THE STRING + NEW AUTOMAT
+                }
+            }
+        }
+
+    }
+
+}
+
+
