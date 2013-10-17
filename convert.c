fc229c0320fa5faa12bb2d07f0242e867b72f6ee
BetaRavener <dark-raven3@hotmail.com>
2013-10-17 23:07:47 +0200

    Formating.

--- convert.c
+++ convert.c
@@ -5,20 +5,20 @@
 
 int64_t stringToInt(const String *str)
 {
-    int64_t tmp = 0;
+    int64_t tmp;
     char* endptr = 0;
     tmp = strtoll(str->data, &endptr, 10);
-    if(endptr == str->data)
+    if (endptr == str->data)
         setError(ERR_Convert);
     return tmp;
 }
 
 double stringToDouble(const String *str)
 {
-    double tmp = 0.0;
+    double tmp;
     char* endptr = 0;
     tmp = strtod(str->data, &endptr);
-    if(endptr == str->data)
+    if (endptr == str->data)
         setError(ERR_Convert);
     return tmp;
 }
@@ -27,15 +27,15 @@ String *intToString(int64_t num)
 {
     String *tmp;
     /* Buffer size explanation.
-    * Unsigned:
-    * log10(2^64) = 19.2 => 20 + 1 (null terminator)
-    * Signed:
-    * log10(2^63) = 18.9 => 19 + 1 (sign) + 1 (null terminator)
-    * 21 chars needed at most.
-    */
+     * Unsigned:
+     * log10(2^64) = 19.2 => 20 + 1 (null terminator)
+     * Signed:
+     * log10(2^63) = 18.9 => 19 + 1 (sign) + 1 (null terminator)
+     * 21 chars needed at most.
+     */
     char buffer[21];
     int written = sprintf(buffer, "%lld", (long long int)num);
-    if(written > 0)
+    if (written > 0)
         tmp = newStringS(buffer, written);
     else
         setError(ERR_Convert);
@@ -47,15 +47,15 @@ String *doubleToString(double num, uint8_t precision)
 {
     String *tmp = newString();
     /* Buffer size explanation.
-    * Sign: 1
-    * Mantisa: 15-17 digits => 17 + 1 (dot)
-    * Exponent: (1 (e / E) + 1 (sign) + log10(2^10)) = 6
-    * Null terminator: 1
-    * 26 chars needed at most.
-    */
+     * Sign: 1
+     * Mantisa: 15-17 digits => 17 + 1 (dot)
+     * Exponent: (1 (e / E) + 1 (sign) + log10(2^10)) = 6
+     * Null terminator: 1
+     * 26 chars needed at most.
+     */
     char buffer[26];
     int written = sprintf(buffer, "%.*e", precision, num);
-    if(written > 0)
+    if (written > 0)
         tmp = newStringS(buffer, written);
     else
         setError(ERR_Convert);
