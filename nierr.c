0852fda75ae5967f821c2d8f4cc5b7ae3d17c67c
BetaRavener <dark-raven3@hotmail.com>
2013-11-27 19:37:45 +0100

    Improved error printing, fixed expression parsing.

--- nierr.c
+++ nierr.c
@@ -3,23 +3,137 @@
 NiError niErr;
 
 #ifdef DEBUG
+#include "token_vector.h"
 #include <unistd.h>
 
-#define STD_ERR_INT   2
-#define NORMAL_COLOR  "\033[0m"
-#define ERR_COLOR     "\033[1m\033[31m"
+#define STD_ERR_INT     2
+#define MAX_TOKEN_PEEK  4
+#define NORMAL_COLOR    "\033[0m"
+#define ERR_COLOR       "\033[1m\033[31m"
+#define INFO_COLOR      "\033[1m\033[33m"
 
 char errorPrinted = 1;
+
+extern ConstTokenVectorIterator tokensIt;
+
+void printToken(const Token *token) {
+    switch (token->type) {
+        case STT_Plus:
+            fprintf(stderr, "+"); break;
+        case STT_Minus:
+            fprintf(stderr, "-"); break;
+        case STT_Multiply:
+            fprintf(stderr, "*"); break;
+        case STT_Divide:
+            fprintf(stderr, "/"); break;
+        case STT_Dot:
+            fprintf(stderr, "."); break;
+        case STT_Less:
+            fprintf(stderr, "<"); break;
+        case STT_Greater:
+            fprintf(stderr, ">"); break;
+        case STT_LessEqual:
+            fprintf(stderr, "<="); break;
+        case STT_GreaterEqual:
+            fprintf(stderr, ">="); break;
+        case STT_Equal:
+            fprintf(stderr, "==="); break;
+        case STT_NotEqual:
+            fprintf(stderr, "!=="); break;
+        case STT_LeftBracket:
+            fprintf(stderr, "("); break;
+        case STT_RightBracket:
+            fprintf(stderr, ")"); break;
+        case STT_Identifier:
+            fprintf(stderr, "%s", token->str.data); break;
+        case STT_Comma:
+            fprintf(stderr, ","); break;
+        case STT_EOF:
+            fprintf(stderr, "EOF"); break;
+        case STT_Variable:
+            fprintf(stderr, "$%s", token->str.data); break;
+        case STT_Number:
+            fprintf(stderr, "%d", token->n); break;
+        case STT_Double:
+            fprintf(stderr, "%g", token->d); break;
+        case STT_Bool:
+            fprintf(stderr, "%s", token->n == 0 ? "false" : "true"); break;
+        case STT_Null:
+            fprintf(stderr, "null"); break;
+        case STT_String:
+            fprintf(stderr, "\"%s\"", token->str.data); break;
+        case STT_Semicolon:
+            fprintf(stderr, ";"); break;
+        case STT_LeftCurlyBracket:
+            fprintf(stderr, "{"); break;
+        case STT_RightCurlyBracket:
+            fprintf(stderr, "}"); break;
+        case STT_Empty:
+            break;
+        case STT_Keyword:
+            switch (token->keywordType) {
+                case KTT_If:
+                    fprintf(stderr, "if"); break;
+                case KTT_Else:
+                    fprintf(stderr, "else"); break;
+                case KTT_Elseif:
+                    fprintf(stderr, "elseif"); break;
+                case KTT_Continue:
+                    fprintf(stderr, "continue"); break;
+                case KTT_Break:
+                    fprintf(stderr, "break"); break;
+                case KTT_While:
+                    fprintf(stderr, "while"); break;
+                case KTT_For:
+                    fprintf(stderr, "for"); break;
+                case KTT_Return:
+                    fprintf(stderr, "return"); break;
+                case KTT_Function:
+                    fprintf(stderr, "function"); break;
+                default:
+                    break;
+            }
+            break;
+        case STT_Assignment:
+            fprintf(stderr, "="); break;
+        case STT_Php:
+            fprintf(stderr, "<?php"); break;
+        default:
+            break;
+    }
+}
 
 void printErrorF(const char *str)
 {
     if (isatty(STD_ERR_INT)) {
-        fprintf(stderr, ERR_COLOR "Error at %s:%d:%s: %s" NORMAL_COLOR,
+        fprintf(stderr, ERR_COLOR "Error at %s:%d:%s: %s" INFO_COLOR,
                 niErr.file, niErr.line, niErr.fun, str);
     }
     else {
         fprintf(stderr, "Error at %s:%d:%s: %s",
                 niErr.file, niErr.line, niErr.fun, str);
+    }
+
+    if (niErr.type == ERR_Syntax && (tokensIt != NULL)) {
+        fprintf(stderr, "Tokens around error:\n");
+        ConstTokenVectorIterator beginIt = tokensIt;
+        ConstTokenVectorIterator endIt = tokensIt;
+
+        for (int i = 0; beginIt->type != STT_Php && i < MAX_TOKEN_PEEK; i++)
+            beginIt--;
+        for (int i = 0; endIt->type != STT_EOF && i < MAX_TOKEN_PEEK; i++)
+            endIt++;
+        endIt++; // To point behind last token
+
+        for (;beginIt != endIt; beginIt++) {
+            printToken(beginIt);
+            fprintf(stderr, " ");
+        }
+        fprintf(stderr, "\n");
+    }
+
+    if (isatty(STD_ERR_INT)) {
+        fprintf(stderr, NORMAL_COLOR);
     }
 }
 
